2c3126700225da56bdc619b866d039a7e1a3dec5
khajamainoddin <khajamainoddin.mohd@gmail.com>
2017-01-16 15:32:09 +0530

    Review Scaffold

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,55 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  respond_to :html
+
+  def index
+    @reviews = Review.all
+    respond_with(@reviews)
+  end
+
+  def show
+
+  end
+
+  def new
+    @review = Review.new
+    respond_with(@review)
+  end
+
+  def edit
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    #@review.save
+    #respond_with(@review)
+    if @review.save
+      redirect_to @review
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @review.update(review_params)
+    respond_with(@review)
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to root_path
+    #respond_with(@review)
+
+  end
+
+  private
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+end
